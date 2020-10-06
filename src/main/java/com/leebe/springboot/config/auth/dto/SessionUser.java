293f2bb7135b348a7621c4efeee0046697f8bbb8
hanae <doithanae@gmail.com>
2020-10-06 22:26:09 +0900

    ~194 구글 로그인 구현

---
+++ src/main/java/com/leebe/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,17 @@
+package com.leebe.springboot.config.auth.dto;
+
+import com.leebe.springboot.domain.user.User;
+import lombok.Getter;
+
+@Getter
+public class SessionUser {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
