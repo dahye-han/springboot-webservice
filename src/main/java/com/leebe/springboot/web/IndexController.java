20efb233eeda62ed539daf71ace39512aa3a47b9
hanae <doithanae@gmail.com>
2020-10-07 23:27:51 +0900

    ~202

--- src/main/java/com/leebe/springboot/web/IndexController.java
+++ src/main/java/com/leebe/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.leebe.springboot.web;
 
+import com.leebe.springboot.config.auth.LoginUser;
 import com.leebe.springboot.config.auth.dto.SessionUser;
 import com.leebe.springboot.service.posts.PostsService;
 import com.leebe.springboot.web.dto.PostsResponseDto;
@@ -19,12 +20,13 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-        if(user != null) {
+
+        if( user != null ){
             model.addAttribute("userName", user.getName());
         }
+
         return "index";
     }
 
