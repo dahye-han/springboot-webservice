66157e02bf6a00a9988ba18364341577fd8864d2
hanae <doithanae@gmail.com>
2020-10-08 22:23:39 +0900

    ~210

--- src/main/java/com/leebe/springboot/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/leebe/springboot/config/auth/CustomOAuth2UserService.java
@@ -37,7 +37,7 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
         httpSession.setAttribute("user", new SessionUser(user));
 
         return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())),
-                attributes.getAttributes(), attributes.getNameAttributekey());
+                attributes.getAttributes(), attributes.getNameAttributeKey());
     }
 
     private User saveOrUpdate(OAuthAttributes attributes) {
--- src/main/java/com/leebe/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/leebe/springboot/config/auth/dto/OAuthAttributes.java
@@ -7,24 +7,29 @@ import lombok.Getter;
 
 import java.util.Map;
 
+
 @Getter
 public class OAuthAttributes {
     private Map<String, Object> attributes;
-    private String nameAttributekey;
+    private String nameAttributeKey;
     private String name;
     private String email;
     private String picture;
 
     @Builder
-    public OAuthAttributes(Map<String, Object> attributes, String nameAttributekey, String name, String email, String picture) {
+    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture) {
         this.attributes = attributes;
-        this.nameAttributekey = nameAttributekey;
+        this.nameAttributeKey = nameAttributeKey;
         this.name = name;
         this.email = email;
         this.picture = picture;
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
+
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -34,7 +39,20 @@ public class OAuthAttributes {
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
-                .nameAttributekey(userNameAttributeName)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
                 .build();
     }
 
