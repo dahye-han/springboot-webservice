1c7739abad598887d2b337f4eaf0af41dc915867
hanae <doithanae@gmail.com>
2020-10-14 23:46:40 +0900

    Serializable

--- src/main/java/com/leebe/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/com/leebe/springboot/config/auth/dto/SessionUser.java
@@ -3,8 +3,13 @@ package com.leebe.springboot.config.auth.dto;
 import com.leebe.springboot.domain.user.User;
 import lombok.Getter;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
